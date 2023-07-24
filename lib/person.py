68edac727faf30473610e81b4745b365b1597f7d
Yisak Girmay <yisakbruck23@gmail.com>
2023-07-23 20:05:19 -0400

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -1,23 +1,3 @@
-# #!/usr/bin/env python3
-
-# APPROVED_JOBS = [
-#     "Admin",
-#     "Customer Service",
-#     "Human Resources",
-#     "ITC",
-#     "Production",
-#     "Legal",
-#     "Finance",
-#     "Sales",
-#     "General Management",
-#     "Research & Development",
-#     "Marketing",
-#     "Purchasing"
-# ]
-
-# class Person:
-    
-#     pass
 
 
 # APPROVED_JOBS = [
@@ -35,32 +15,33 @@
 #     "Purchasing"
 # ]
 
-# class Person:
-#     def __init__(self, name="", job=""):
-#         self.name = name
-#         self.job = job
+# # class Person:
+# #     def __init__(self, name="", job=""):
+# #         self.name = name
+# #         self.job = job
 
-#     def get_name(self):
-#         return self._name
+# #     def get_name(self):
+# #         return self._name
 
-#     def set_name(self, name):
-#         if isinstance(name, str) and 1 <= len(name) <= 25:
-#             self._name = name.title()
-#         else:
-#             print("Name must be a string between 1 and 25 characters.")
+# #     def set_name(self, name):
+# #         if (type(name) == str) and 1 <= len(name) <= 25:
+# #             self._name = name.title()
+# #         else:
+# #             print("Name must be string between 1 and 25 characters.")
 
-#     name = property(get_name, set_name)
+# #     name = property(get_name, set_name)
 
-#     def get_job(self):
-#         return self._job
+# #     def get_job(self):
+# #         return self._job
 
-#     def set_job(self, job):
-#         if job in APPROVED_JOBS:
-#             self._job = job
-#         else:
-#             print("Job must be in the list of approved jobs.")
+# #     def set_job(self, job):
+# #         if job in APPROVED_JOBS:
+# #             self._job = job
+# #         else:
+# #             print("Job must be in list of approved jobs.")
 
-#     job = property(get_job, set_job)
+# #     job = property(get_job, set_job)
+
 
 APPROVED_JOBS = [
     "Admin",
@@ -78,9 +59,11 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    def __init__(self, name="", job=""):
-        self.name = name
-        self.job = job
+    def __init__(self, name=None, job=None):
+        if name is not None:
+            self.name = name
+        if job is not None:
+            self.job = job
 
     def get_name(self):
         return self._name
@@ -89,7 +72,7 @@ class Person:
         if (type(name) == str) and 1 <= len(name) <= 25:
             self._name = name.title()
         else:
-            print("Name must be a string between 1 and 25 characters.")
+            print("Name must be string between 1 and 25 characters.")
 
     name = property(get_name, set_name)
 
@@ -100,6 +83,6 @@ class Person:
         if job in APPROVED_JOBS:
             self._job = job
         else:
-            print("Job must be in the list of approved jobs.")
+            print("Job must be in list of approved jobs.")
 
     job = property(get_job, set_job)
